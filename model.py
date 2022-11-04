fd1ab0f0c514299dd965f41781c6a78250233286
salmareda104 <100859819+salmareda104@users.noreply.github.com>
2022-11-04 12:42:39 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+st.title('Customer Churn Prediction')
+
+Contract = st.selectbox('contract type' , ('Month-to-Month','One year','Two year'))
+
+st.write('you choice:',Contract)
+                      
+
+
+
+
+
+
+
+
